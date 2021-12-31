7408b63d0925bc34b76590342b826f6ba56fa684
manoj shirgire <manojshirgire@gmail.com>
2021-12-31 13:21:06 +0530

    tree

--- tree/Tree.java
+++ tree/Tree.java
@@ -1,4 +1,46 @@
 package tree;
 
+import java.util.Scanner;
+
 public class Tree {
+
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        createTree();
+    }
+
+    private static Node createTree() {
+
+        Node node = null;
+
+        System.out.println("Enter data");
+        int data = scanner.nextInt();
+
+        //base condition
+        if(data == -1) return null;
+
+        node = new Node(data);
+
+        System.out.println("Enter data for left of "+data);
+        node.left = createTree();
+
+        System.out.println("Enter data for right of "+data);
+        node.right =createTree();
+
+        return node;
+
+
+    }
+}
+
+
+class Node{
+
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data){
+        this.data = data;
+    }
 }
